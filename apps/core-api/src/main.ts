860542cc010e1f0399e4821eba8f068a3ec91f4c
Basil Earle <basilearle@gmail.com>
2025-10-27 19:24:58 -0300

    initial hono setup

--- apps/core-api/src/main.ts
+++ apps/core-api/src/main.ts
@@ -1,1 +1,32 @@
-console.log('Hello World');
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+
+const PORT = process.env.PORT || 3000;
+
+const app = new Hono();
+
+app.get('/', (c) => c.text('Hello Node.js!'));
+
+const server = serve({
+  fetch: app.fetch,
+  port: +PORT,
+}, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// graceful shutdown
+process.on('SIGINT', () => {
+  server.close();
+  process.exit(0);
+});
+
+process.on('SIGTERM', () => {
+  server.close((err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+
+    process.exit(0);
+  });
+});

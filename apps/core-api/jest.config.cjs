697391aa41aa7e0489a4eeb05bded9cf3902a02e
Basil Earle <basilearle@gmail.com>
2025-10-27 19:14:04 -0300

    created core-api app

---
+++ apps/core-api/jest.config.cjs
@@ -0,0 +1,21 @@
+// NOTE: file must be .cjs for now due to https://github.com/nrwl/nx/issues/32236
+const { readFileSync } = require('fs');
+
+// Reading the SWC compilation config for the spec files
+const swcJestConfig = JSON.parse(
+  readFileSync(`${__dirname}/.spec.swcrc`, 'utf-8')
+);
+
+// Disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves
+swcJestConfig.swcrc = false;
+
+module.exports = {
+  displayName: '@blameable/core-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: 'test-output/jest/coverage',
+};

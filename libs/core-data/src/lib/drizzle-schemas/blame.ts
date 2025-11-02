dc9b646dde4092f50958cdbe16f1be7aecec8a71
Basil Earle <basilearle@gmail.com>
2025-11-02 14:19:28 -0400

    feat: added relationships

--- libs/core-data/src/lib/drizzle-schemas/blame.ts
+++ libs/core-data/src/lib/drizzle-schemas/blame.ts
@@ -1,7 +1,13 @@
 import { date, pgTable, uuid, varchar } from 'drizzle-orm/pg-core';
 
+import { sitesTable } from './sites';
+
 export const blameRegisterTable = pgTable('blame_register', {
   id: uuid().primaryKey().defaultRandom(),
+  site_id: uuid()
+    .notNull()
+    .unique()
+    .references(() => sitesTable.id, { onDelete: 'cascade' }),
   ip_address: varchar({ length: 255 }).notNull(),
   created_at: date().defaultNow().notNull(),
 });
--- libs/core-data/src/lib/drizzle-schemas/cms.ts
+++ libs/core-data/src/lib/drizzle-schemas/cms.ts
@@ -1,7 +1,13 @@
 import { date, pgTable, uuid } from 'drizzle-orm/pg-core';
 
+import { sitesTable } from './sites';
+
 export const cmsTokensTable = pgTable('cms_tokens', {
   id: uuid().primaryKey().defaultRandom(),
+  site_id: uuid()
+    .notNull()
+    .unique()
+    .references(() => sitesTable.id, { onDelete: 'cascade' }),
   created_at: date().defaultNow().notNull(),
   updated_at: date().defaultNow().notNull(),
 });

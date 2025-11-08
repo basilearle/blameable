8638c7536da05cf46e1a38f25a5c7c45d956561c
Basil Earle <basilearle@gmail.com>
2025-11-07 23:18:23 -0400

    feat: add admin-ui boilerplate

---
+++ apps/admin-ui/src/main.tsx
@@ -0,0 +1,21 @@
+import "@radix-ui/themes/styles.css";
+
+// ---
+
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+
+import App from './app/App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);

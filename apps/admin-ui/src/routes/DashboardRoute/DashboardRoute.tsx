003c9a923da62dfff384e7072a64751300bf4ef1
Basil Earle <basilearle@gmail.com>
2025-11-10 10:51:32 -0400

    fix: resolved runtime error on Dashboard route

--- apps/admin-ui/src/routes/DashboardRoute/DashboardRoute.tsx
+++ apps/admin-ui/src/routes/DashboardRoute/DashboardRoute.tsx
@@ -5,15 +5,17 @@ import { DashboardLoaderData } from './DashboardRoute.loader';
 export function DashboardRoute() {
   const { data } = useLoaderData<DashboardLoaderData>();
 
+  const siteList = data?.sites?.map((site) => (
+    <li key={site.id}>{site.name}</li>
+  ));
+
   return (
     <div>
       <h2>This is the Dashboard.</h2>
 
       <h3>Here are your sites:</h3>
       <ul>
-        {data?.sites.map((site) => (
-          <li key={site.id}>{site.name}</li>
-        ))}
+        {siteList || <li>No sites found.</li>}
       </ul>
     </div>
   );

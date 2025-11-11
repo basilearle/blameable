246085f3dc5bd44fff5bd15572b9cc75ca88e560
Basil Earle <basilearle@gmail.com>
2025-11-11 00:02:21 -0400

    feat: docker containers now forward the site id

--- apps/portal-ui/docker-entrypoint.sh
+++ apps/portal-ui/docker-entrypoint.sh
@@ -2,8 +2,9 @@
 set -e
 
 echo "traffic prefixed with '/api' will be forwarded to: ${API_URL}"
+echo "site id is: ${API_SITE_ID}"
 
-envsubst '${API_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${API_URL} ${API_SITE_ID}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 echo "starting interpolated nginx..."
 

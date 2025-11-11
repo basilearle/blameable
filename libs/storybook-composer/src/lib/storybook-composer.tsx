9fbd8d374b32a5942892be27b39dfbc56bc07acc
Basil Earle <basilearle@gmail.com>
2025-11-11 14:49:17 -0400

    feat: create storybook composer library

---
+++ libs/storybook-composer/src/lib/storybook-composer.tsx
@@ -0,0 +1,11 @@
+import styles from './storybook-composer.module.css';
+
+export function BlameableStorybookComposer() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to BlameableStorybookComposer!</h1>
+    </div>
+  );
+}
+
+export default BlameableStorybookComposer;

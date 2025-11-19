146dba3671e76a142b3d3bc26283881ddaf45bd6
Basil Earle <basilearle@gmail.com>
2025-11-18 21:35:07 -0400

    chore: added global column with unique constraint to sites table

--- libs/core-data/src/lib/drizzle-schemas/sites.ts
+++ libs/core-data/src/lib/drizzle-schemas/sites.ts
@@ -1,18 +1,36 @@
-import { char, pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import { sql } from 'drizzle-orm';
+import {
+  boolean,
+  char,
+  pgTable,
+  timestamp,
+  uniqueIndex,
+  uuid,
+  varchar,
+} from 'drizzle-orm/pg-core';
 
-export const sites = pgTable('sites', {
-  id: uuid()
-    .primaryKey()
-    .defaultRandom(),
-  name: varchar({ length: 50, })
-    .notNull(),
-  defaultLocale: char('default_locale', { length: 5, })
-    .notNull(),
-  createdAt: timestamp('created_at')
-    .notNull()
-    .defaultNow(),
-  updatedAt: timestamp('updated_at')
-    .notNull()
-    .defaultNow()
-    .$onUpdate(() => /* @__PURE__ */ new Date()),
-});
+export const sites = pgTable(
+  'sites',
+  {
+    id: uuid()
+      .primaryKey()
+      .defaultRandom(),
+    name: varchar({ length: 50 })
+      .notNull(),
+    defaultLocale: char('default_locale', { length: 5 })
+      .notNull(),
+    global: boolean('global')
+      .notNull()
+      .default(false),
+    createdAt: timestamp('created_at')
+      .notNull()
+      .defaultNow(),
+    updatedAt: timestamp('updated_at')
+      .notNull()
+      .defaultNow()
+      .$onUpdate(() => /* @__PURE__ */ new Date()),
+  },
+  (table) => [
+    uniqueIndex('one_global_site').on(table.global).where(sql`${table.global} = true`),
+  ],
+);
